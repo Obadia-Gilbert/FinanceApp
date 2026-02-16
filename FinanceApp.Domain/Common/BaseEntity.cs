bdb9b0111a7971c18d5ab50a0a651bccc55f1c92
Obadia Gilbert <gilbertobadia830@gmail.com>
2026-02-16 19:24:50 +0300

    Designed Domain Expenses, BaseEntity and Currency

---
+++ FinanceApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,27 @@
+namespace FinanceApp.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+
+    public DateTimeOffset CreatedAt { get; private set; }
+
+    public DateTimeOffset? UpdatedAt { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+
+    protected void SetCreated(DateTimeOffset createdAt)
+    {
+        CreatedAt = createdAt;
+    }
+
+    public void MarkAsUpdated(DateTimeOffset updatedAt)
+    {
+        UpdatedAt = updatedAt;
+    }
+
+    public void SoftDelete()
+    {
+        IsDeleted = true;
+    }
+}
